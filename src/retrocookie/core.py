5b8e14dc254a043628043a491c6c4cd74fa8c290
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-05-22 22:41:01 +0200

    Move git interface to separate module

--- src/retrocookie/core.py
+++ src/retrocookie/core.py
@@ -10,57 +10,11 @@ from typing import List
 from typing import Optional
 from typing import Tuple
 
+from . import git
+
 
 NAMESPACE = "retrocookie"
 REMOTE = "retrocookie-instance"
-
-
-def exists_remote(remote: str) -> bool:
-    """Return True if the remote exists."""
-    process = subprocess.run(
-        ["git", "remote"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        universal_newlines=True,
-        check=True,
-    )
-    remotes = process.stdout.split()
-    return remote in remotes
-
-
-def add_remote(remote: str, url: str) -> None:
-    """Add the remote with the given URL. Disallow push."""
-    subprocess.run(["git", "remote", "add", remote, url], check=True)
-    subprocess.run(["git", "remote", "set-url", "--push", remote, "none"], check=True)
-
-
-def remove_remote(remote: str) -> None:
-    """Remove the remote."""
-    subprocess.run(["git", "remote", "remove", remote], check=True)
-
-
-def get_remote_url(remote: str) -> str:
-    """Return the URL of the remote."""
-    process = subprocess.run(
-        ["git", "remote", "get-url", remote],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        universal_newlines=True,
-        check=True,
-    )
-    return process.stdout.strip()
-
-
-def fetch_remote(remote: str, ref: str) -> None:
-    """Fetch ref from the remote."""
-    subprocess.run(["git", "fetch", "--no-tags", remote, ref], check=True)
-
-
-def create_branch(branch: str, remote: str, ref: str) -> None:
-    """Create a local branch for the remote ref. Reset it if it exists."""
-    subprocess.run(
-        ["git", "switch", "--force-create", branch, f"{REMOTE}/{ref}"], check=True,
-    )
 
 
 def find_template_directory() -> Path:
@@ -98,6 +52,41 @@ def get_replacements(
     return replacements
 
 
+def guess_remote_url() -> str:
+    """Guess the URL of the template instance."""
+    url = git.get_remote_url("origin")
+    if url.endswith(".git"):
+        url = url[: -len(".git")]
+    return f"{url}-instance.git"
+
+
+def retrocookie(
+    url: Optional[str], ref: str, whitelist: Container[str], blacklist: Container[str],
+) -> None:
+    """Import commits from instance repository into template repository."""
+    if url is None:
+        url = guess_remote_url()
+
+    template_directory = find_template_directory()
+    original_branch = git.get_current_branch()
+    branch = f"{NAMESPACE}/{ref}"
+
+    if git.exists_remote(REMOTE):
+        git.remove_remote(REMOTE)
+
+    try:
+        git.add_remote(REMOTE, url)
+        git.fetch_remote(REMOTE, ref)
+        git.create_branch(branch, REMOTE, ref)
+        context = load_context()
+        replacements = get_replacements(context, whitelist, blacklist)
+        filter_branch(branch, template_directory, replacements)
+        git.switch_branch(original_branch)
+    finally:
+        if git.exists_remote(REMOTE):
+            git.remove_remote(REMOTE)
+
+
 def filter_branch(
     branch: str, template_directory: Path, replacements: List[Tuple[str, str]]
 ) -> None:
@@ -121,99 +110,16 @@ def filter_branch(
         subprocess.run(command, check=True)
 
 
-def get_current_branch() -> str:
-    """Return the current branch."""
-    process = subprocess.run(
-        ["git", "rev-parse", "--abbrev-ref", "HEAD"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        universal_newlines=True,
-        check=True,
-    )
-    return process.stdout.strip()
-
-
-def switch_branch(branch: str) -> None:
-    """Switch the current branch."""
-    subprocess.run(["git", "switch", branch], check=True)
-
-
-def guess_remote_url() -> str:
-    """Guess the URL of the template instance."""
-    url = get_remote_url("origin")
-    if url.endswith(".git"):
-        url = url[: -len(".git")]
-    return f"{url}-instance.git"
-
-
-def retrocookie(
-    url: Optional[str], ref: str, whitelist: Container[str], blacklist: Container[str],
-) -> None:
-    """Import commits from instance repository into template repository."""
-    if url is None:
-        url = guess_remote_url()
-
-    template_directory = find_template_directory()
-    original_branch = get_current_branch()
-    branch = f"{NAMESPACE}/{ref}"
-
-    if exists_remote(REMOTE):
-        remove_remote(REMOTE)
-
-    try:
-        add_remote(REMOTE, url)
-        fetch_remote(REMOTE, ref)
-        create_branch(branch, REMOTE, ref)
-        context = load_context()
-        replacements = get_replacements(context, whitelist, blacklist)
-        filter_branch(branch, template_directory, replacements)
-        switch_branch(original_branch)
-    finally:
-        if exists_remote(REMOTE):
-            remove_remote(REMOTE)
-
-
-def exists_branch(branch: str) -> bool:
-    """Return True if the branch exists."""
-    process = subprocess.run(
-        ["git", "show-ref", "--verify", "--quiet", "refs/heads/{branch}"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        universal_newlines=True,
-    )
-    return process.returncode == 0
-
-
-def remove_branch(branch: str) -> None:
-    """Remove the branch."""
-    subprocess.run(["git", "branch", "--delete", "--force", branch], check=True)
-
-
-def find_branches() -> List[str]:
-    """Find branches created by this program."""
-    process = subprocess.run(
-        [
-            "git",
-            "for-each-ref",
-            "--format=%(refname:short)",
-            f"refs/heads/{NAMESPACE}/",
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        universal_newlines=True,
-        check=True,
-    )
-    return process.stdout.split()
-
-
 def cleanup(branch: Optional[str]) -> None:
     """Remove branches and remotes created by this program."""
     branches = (
-        [branch] if branch is not None and exists_branch(branch) else find_branches()
+        [branch]
+        if branch is not None and git.exists_branch(branch)
+        else git.find_branches(NAMESPACE)
     )
 
     for branch in branches:
-        remove_branch(branch)
+        git.remove_branch(branch)
 
-    if exists_remote(REMOTE):
-        remove_remote(REMOTE)
+    if git.exists_remote(REMOTE):
+        git.remove_remote(REMOTE)
---
+++ src/retrocookie/git.py
@@ -0,0 +1,101 @@
+"""Git interface."""
+import subprocess  # noqa: S404
+from typing import List
+
+
+def exists_remote(remote: str) -> bool:
+    """Return True if the remote exists."""
+    process = subprocess.run(
+        ["git", "remote"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+        check=True,
+    )
+    remotes = process.stdout.split()
+    return remote in remotes
+
+
+def add_remote(remote: str, url: str) -> None:
+    """Add the remote with the given URL. Disallow push."""
+    subprocess.run(["git", "remote", "add", remote, url], check=True)
+    subprocess.run(["git", "remote", "set-url", "--push", remote, "none"], check=True)
+
+
+def remove_remote(remote: str) -> None:
+    """Remove the remote."""
+    subprocess.run(["git", "remote", "remove", remote], check=True)
+
+
+def get_remote_url(remote: str) -> str:
+    """Return the URL of the remote."""
+    process = subprocess.run(
+        ["git", "remote", "get-url", remote],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+        check=True,
+    )
+    return process.stdout.strip()
+
+
+def fetch_remote(remote: str, ref: str) -> None:
+    """Fetch ref from the remote."""
+    subprocess.run(["git", "fetch", "--no-tags", remote, ref], check=True)
+
+
+def create_branch(branch: str, remote: str, ref: str) -> None:
+    """Create a local branch for the remote ref. Reset it if it exists."""
+    subprocess.run(
+        ["git", "switch", "--force-create", branch, f"{remote}/{ref}"], check=True,
+    )
+
+
+def exists_branch(branch: str) -> bool:
+    """Return True if the branch exists."""
+    process = subprocess.run(
+        ["git", "show-ref", "--verify", "--quiet", "refs/heads/{branch}"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    return process.returncode == 0
+
+
+def remove_branch(branch: str) -> None:
+    """Remove the branch."""
+    subprocess.run(["git", "branch", "--delete", "--force", branch], check=True)
+
+
+def get_current_branch() -> str:
+    """Return the current branch."""
+    process = subprocess.run(
+        ["git", "rev-parse", "--abbrev-ref", "HEAD"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+        check=True,
+    )
+    return process.stdout.strip()
+
+
+def switch_branch(branch: str) -> None:
+    """Switch the current branch."""
+    subprocess.run(["git", "switch", branch], check=True)
+
+
+def find_branches(namespace: str) -> List[str]:
+    """Find branches under the given namespace."""
+    process = subprocess.run(
+        [
+            "git",
+            "for-each-ref",
+            "--format=%(refname:short)",
+            f"refs/heads/{namespace}/",
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+        check=True,
+    )
+    return process.stdout.split()
