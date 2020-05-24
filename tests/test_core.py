b62777ada30300dc4e7362f3ec06d6922659b8cf
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-05-25 00:04:37 +0200

    Remove redundant tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,23 +3,7 @@ from retrocookie import git
 from retrocookie import retrocookie
 
 
-def test_cookiecutter_repository(cookiecutter_repository: git.Repository) -> None:
-    """It succeeds."""
-    repository = cookiecutter_repository
-    repository.git("show")
-    repository.git("status")
-
-
-def test_cookiecutter_instance(
-    cookiecutter_instance_repository: git.Repository,
-) -> None:
-    """It succeeds."""
-    repository = cookiecutter_instance_repository
-    repository.git("show")
-    repository.git("status")
-
-
-def test_cookiecutter_instance_commit(
+def test_core(
     cookiecutter_repository: git.Repository,
     cookiecutter_instance_repository_with_topic: git.Repository,
 ) -> None:
