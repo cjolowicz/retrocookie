187afa13ba827f3366d3503aa8e34706bfd08a03
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-05-24 19:48:54 +0200

    Add test for retrocookie

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,8 +1,34 @@
 """Tests for core module."""
 from retrocookie import git
+from retrocookie import retrocookie
 
 
-def test_core(cookiecutter_repository: git.Repository) -> None:
+def test_cookiecutter_repository(cookiecutter_repository: git.Repository) -> None:
     """It succeeds."""
-    cookiecutter_repository.git("show")
-    cookiecutter_repository.git("status")
+    repository = cookiecutter_repository
+    repository.git("show")
+    repository.git("status")
+
+
+def test_cookiecutter_instance(
+    cookiecutter_instance_repository: git.Repository,
+) -> None:
+    """It succeeds."""
+    repository = cookiecutter_instance_repository
+    repository.git("show")
+    repository.git("status")
+
+
+def test_cookiecutter_instance_commit(
+    cookiecutter_repository: git.Repository,
+    cookiecutter_instance_repository_with_topic: git.Repository,
+) -> None:
+    """It succeeds."""
+    path = cookiecutter_repository.path
+    url = str(cookiecutter_instance_repository_with_topic.path)
+
+    retrocookie("topic", path=path, url=url)
+    cookiecutter_repository.switch_branch("topic")
+
+    readme = path / "README.md"
+    assert "Lorem ipsum" in readme.read_text()
