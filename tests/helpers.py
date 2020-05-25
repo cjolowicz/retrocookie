84f0ef79689ada0ac2312be4fe65c92cbb71b835
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-05-25 23:12:01 +0200

    Add missing docstrings to test helpers

--- tests/helpers.py
+++ tests/helpers.py
@@ -11,22 +11,26 @@ AUTHOR_EMAIL = "user@example.com"
 
 
 def read(repository: git.Repository, path: Path) -> str:
+    """Read file in repository."""
     path = repository.path / path
     return path.read_text()
 
 
 def write(repository: git.Repository, path: Path, text: str) -> None:
+    """Write file in repository."""
     path = repository.path / path
     path.write_text(text)
 
 
 def append(repository: git.Repository, path: Path, text: str) -> None:
+    """Append to file in repository."""
     text = read(repository, path) + text
     write(repository, path, text)
 
 
 @contextlib.contextmanager
 def branch(repository: git.Repository, branch: str) -> Iterator[None]:
+    """Switch to branch, creating it if needed."""
     original = repository.get_current_branch()
 
     if not repository.exists_branch(branch):
@@ -41,6 +45,7 @@ def branch(repository: git.Repository, branch: str) -> Iterator[None]:
 
 
 def commit(repository: git.Repository, path: Path) -> None:
+    """Create a commit with the path."""
     repository.add(path)
     repository.commit(
         author=AUTHOR, author_email=AUTHOR_EMAIL, message=f"Update {path}"
@@ -48,4 +53,5 @@ def commit(repository: git.Repository, path: Path) -> None:
 
 
 def in_template(path: Path) -> Path:
+    """Prepend the template directory to the path."""
     return "{{ cookiecutter.project_slug }}" / path
