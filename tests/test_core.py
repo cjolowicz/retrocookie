8407cc642d6ac297e5e95f4a12eb14c86b8596d7
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-05-25 15:36:17 +0200

    Add test for core.guess_instance_url

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,8 @@
 """Tests for core module."""
 from pathlib import Path
 
+import pytest
+from retrocookie import core
 from retrocookie import git
 from retrocookie import retrocookie
 
@@ -24,3 +26,9 @@ def test_append(
 
     with branch(cookiecutter, "topic"):
         assert text in read(cookiecutter, in_template(path))
+
+
+def test_guess_instance_url(cookiecutter_instance_repository: git.Repository) -> None:
+    """It raises an exception when there is no template directory."""
+    with pytest.raises(Exception):
+        core.guess_instance_url(cookiecutter_instance_repository)
