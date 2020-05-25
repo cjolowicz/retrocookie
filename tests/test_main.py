468411e1652ab19d0909d60d8fdc84e2d7b839ae
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-05-25 16:08:52 +0200

    Add test for --local

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 """Test cases for the __main__ module."""
 from pathlib import Path
+from typing import Iterable
 
 import pytest
 from click.testing import CliRunner
@@ -22,10 +23,14 @@ def test_help(runner: CliRunner) -> None:
     assert result.exit_code == 0
 
 
+@pytest.mark.parametrize(
+    "options", [[], ["--local=other"],],
+)
 def test_main(
     runner: CliRunner,
     cookiecutter_repository: git.Repository,
     cookiecutter_instance_repository: git.Repository,
+    options: Iterable[str],
 ) -> None:
     """It exits with a status code of zero."""
     cookiecutter, instance = cookiecutter_repository, cookiecutter_instance_repository
@@ -38,6 +43,8 @@ def test_main(
 
     with utils.chdir(cookiecutter.path):
         result = runner.invoke(
-            __main__.main, ["--ref=topic", f"--url={instance.path}"],
+            __main__.main,
+            ["--ref=topic", f"--url={instance.path}", *options],
+            catch_exceptions=False,
         )
         assert result.exit_code == 0
