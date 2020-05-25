4b01b5b680b7b9632f18257b9d70898f22aae706
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-05-25 15:10:47 +0200

    Add test for __main__.main

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,13 @@
 """Test cases for the __main__ module."""
+from pathlib import Path
+
 import pytest
 from click.testing import CliRunner
-
 from retrocookie import __main__
+from retrocookie import git
+from retrocookie import utils
+
+from .helpers import *
 
 
 @pytest.fixture
@@ -11,7 +16,28 @@ def runner() -> CliRunner:
     return CliRunner()
 
 
-def test_main_succeeds(runner: CliRunner) -> None:
+def test_help(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(__main__.main, ["--help"])
     assert result.exit_code == 0
+
+
+def test_main(
+    runner: CliRunner,
+    cookiecutter_repository: git.Repository,
+    cookiecutter_instance_repository: git.Repository,
+) -> None:
+    """It exits with a status code of zero."""
+    cookiecutter, instance = cookiecutter_repository, cookiecutter_instance_repository
+    path = Path("README.md")
+    text = "Lorem Ipsum\n"
+
+    with branch(instance, "topic"):
+        append(instance, path, text)
+        commit(instance, path)
+
+    with utils.chdir(cookiecutter.path):
+        result = runner.invoke(
+            __main__.main, ["--ref=topic", f"--url={instance.path}"],
+        )
+        assert result.exit_code == 0
