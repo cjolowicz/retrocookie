0a069d0db5ad0931c573a369e465c7a1eac966d8
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-06-02 07:15:06 +0200

    Add git.Repository.cherrypick (#24)
    * Add git.Repository.cherrypick
    
    * Add repository fixture to git tests
    
    * Add test for git.Repository.cherrypick

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,22 +1,62 @@
 """Tests for git interface."""
 from pathlib import Path
 
+import pytest
+
+from .helpers import branch
 from retrocookie import git
 
 
-def test_commit(tmp_path: Path) -> None:
+@pytest.fixture
+def repository(tmp_path: Path) -> git.Repository:
+    """Initialize repository in a temporary directory."""
+    return git.Repository.init(tmp_path)
+
+
+def test_commit(repository: git.Repository) -> None:
     """It creates a commit."""
-    repository = git.Repository.init(tmp_path)
     repository.commit("Empty")
 
 
-def test_read_text(tmp_path: Path) -> None:
+def test_read_text(repository: git.Repository) -> None:
     """It returns the file contents."""
-    path = tmp_path / "README.md"
+    path = repository.path / "README.md"
     path.write_text("# example\n")
 
-    repository = git.Repository.init(tmp_path)
     repository.add(path)
     repository.commit(f"Add {path}")
 
     assert path.read_text() == repository.read_text(path)
+
+
+def test_cherrypick(repository: git.Repository) -> None:
+    """It applies a commit from another branch."""
+    # Add README on master.
+    readme = repository.path / "README.md"
+    readme.write_text("# example\n")
+
+    repository.add()
+    repository.commit("Initial")
+
+    # Add .gitignore on topic.
+    with branch(repository, "topic", create=True):
+        ignore = repository.path / ".gitignore"
+        ignore_text = "*.pyc\n"
+        ignore.write_text(ignore_text)
+
+        repository.add()
+        repository.commit("Ignore *.pyc")
+
+    # Append <message> to README, on master.
+    with readme.open(mode="a") as io:
+        message = "\nHello, world!\n"
+        io.write(message)
+
+    repository.add()
+    repository.commit("Update")
+
+    # Cherry-pick the .gitignore onto master.
+    repository.cherrypick("topic")
+
+    assert ignore_text == repository.read_text(ignore)
+    assert message in repository.read_text(readme)
