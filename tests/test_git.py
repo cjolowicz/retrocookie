30445215904cc3e6d9bfaf27ef29480acd0394fa
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-06-02 06:42:10 +0200

    Add repository fixture to git tests

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,21 +1,27 @@
 """Tests for git interface."""
 from pathlib import Path
 
+import pytest
+
 from retrocookie import git
 
 
-def test_commit(tmp_path: Path) -> None:
+@pytest.fixture
+def repository(tmp_path: Path) -> git.Repository:
+    """Initialize repository in a temporary directory."""
+    return git.Repository.init(tmp_path)
+
+
+def test_commit(repository: git.Repository) -> None:
     """It creates a commit."""
-    repository = git.Repository.init(tmp_path)
     repository.commit("Empty")
 
 
-def test_read_text(tmp_path: Path) -> None:
+def test_read_text(repository: git.Repository) -> None:
     """It returns the file contents."""
-    path = tmp_path / "README.md"
+    path = repository.path / "README.md"
     path.write_text("# example\n")
 
-    repository = git.Repository.init(tmp_path)
     repository.add(path)
     repository.commit(f"Add {path}")
 
