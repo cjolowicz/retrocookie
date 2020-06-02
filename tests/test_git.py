761fae04fe8153f6905e5fd9cf0e0a3f1f214030
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-06-02 05:40:27 +0200

    Add test for git.Repository.read_text

--- tests/test_git.py
+++ tests/test_git.py
@@ -8,3 +8,15 @@ def test_commit(tmp_path: Path) -> None:
     """It creates a commit."""
     repository = git.Repository.init(tmp_path)
     repository.commit("Empty")
+
+
+def test_read_text(tmp_path: Path) -> None:
+    """It returns the file contents."""
+    path = tmp_path / "README.md"
+    path.write_text("# example\n")
+
+    repository = git.Repository.init(tmp_path)
+    repository.add(path)
+    repository.commit(f"Add {path}")
+
+    assert path.read_text() == repository.read_text(path)
