d7fcbd2068ecc5dd24a260937628cd832061b729
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-06-02 07:06:24 +0200

    Add test for git.Repository.cherrypick

--- tests/test_git.py
+++ tests/test_git.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import pytest
 
+from .helpers import branch
 from retrocookie import git
 
 
@@ -26,3 +27,36 @@ def test_read_text(repository: git.Repository) -> None:
     repository.commit(f"Add {path}")
 
     assert path.read_text() == repository.read_text(path)
+
+
+def test_cherrypick(repository: git.Repository) -> None:
+    """It applies a commit from another branch."""
+    # Add README on master.
+    readme = repository.path / "README.md"
+    readme.write_text("# example\n")
+
+    repository.add()
+    repository.commit("Initial")
+
+    # Add .gitignore on topic.
+    with branch(repository, "topic", create=True):
+        ignore = repository.path / ".gitignore"
+        ignore_text = "*.pyc\n"
+        ignore.write_text(ignore_text)
+
+        repository.add()
+        repository.commit("Ignore *.pyc")
+
+    # Append <message> to README, on master.
+    with readme.open(mode="a") as io:
+        message = "\nHello, world!\n"
+        io.write(message)
+
+    repository.add()
+    repository.commit("Update")
+
+    # Cherry-pick the .gitignore onto master.
+    repository.cherrypick("topic")
+
+    assert ignore_text == repository.read_text(ignore)
+    assert message in repository.read_text(readme)
