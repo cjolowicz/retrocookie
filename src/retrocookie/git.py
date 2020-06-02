f18e7bf9d253aba32dd03c6237a48a7615e224b7
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-06-02 06:33:44 +0200

    Add git.Repository.read_text (#23)
    * Add git.Repository._ensure_relative helper
    
    * Use git.Repository._ensure_relative in git.Repository.add
    
    * Add git.Repository.read_text
    
    * Add test for git.Repository.read_text

--- src/retrocookie/git.py
+++ src/retrocookie/git.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 import subprocess  # noqa: S404
 from pathlib import Path
 from typing import Any
+from typing import cast
 from typing import Optional
 
 import pygit2
@@ -86,12 +87,22 @@ class Repository:
         """Rebase."""
         self.git("rebase", upstream, branch, f"--onto={onto}")
 
+    def _ensure_relative(self, path: Path) -> Path:
+        """Interpret the path relative to the repository root."""
+        return path.relative_to(self.path) if path.is_absolute() else path
+
+    def read_text(self, path: Path, *, ref: str = "HEAD") -> str:
+        """Return the contents of the blob at the given path."""
+        commit = self.repo.references[ref].peel()
+        path = self._ensure_relative(path)
+        blob = commit.tree / str(path)
+        return cast(str, blob.data.decode())
+
     def add(self, *paths: Path) -> None:
         """Add paths to the index."""
         for path in paths:
-            self.repo.index.add(
-                path.relative_to(self.path) if path.is_absolute() else path
-            )
+            path = self._ensure_relative(path)
+            self.repo.index.add(path)
         else:
             self.repo.index.add_all()
         self.repo.index.write()
