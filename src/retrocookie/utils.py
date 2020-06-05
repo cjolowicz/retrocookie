de7f20739b582ced6c7f7631e7c603a244cd96a2
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-06-05 12:50:00 +0200

    Remove unused utils.temporary_remote

--- src/retrocookie/utils.py
+++ src/retrocookie/utils.py
@@ -26,20 +26,3 @@ def temporary_repository(path: Path) -> Iterator[git.Repository]:
     with tempfile.TemporaryDirectory() as tmpdir:
         directory = Path(tmpdir) / "instance"
         yield git.Repository.clone(str(path), directory, mirror=True)
-
-
-@contextlib.contextmanager
-def temporary_remote(
-    repository: git.Repository, remote: str, url: str
-) -> Iterator[None]:
-    """Add remote on entry, remove it on exit."""
-    if repository.exists_remote(remote):
-        repository.remove_remote(remote)
-
-    repository.add_remote(remote, url)
-
-    try:
-        yield
-    finally:
-        if repository.exists_remote(remote):
-            repository.remove_remote(remote)
