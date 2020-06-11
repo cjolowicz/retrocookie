3e4a982b623fe75de26adf3b23b8fa87a70e3bc5
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-06-11 16:25:02 +0200

    Type-check noxfile.py (#91)
    * Move locations variable into mypy session
    
    * Type-check noxfile.py against the Python installation running Nox
    
    Use the --python-executable option to mypy to point the type checker to
    the Python installation running Nox, rather than the virtual environment
    of the Nox session, when type-checking noxfile.py itself. This allows
    mypy to find the type information for Nox, without having to install Nox
    into the session. Nox distributes type information inline and declares
    this fact using the py.typed file specified in PEP 561.
    
    Note that this check is somewhat indeterministic in local testing. The
    Nox version is only pinned in CI. For users with a pre-2020.5.24 version
    of Nox, the session will fail due to missing type hints. The session may
    also conceivably fail in the future if users install a newer version of
    Nox with breaking changes. (In the latter case, the failure serves as a
    useful reminder that noxfile.py needs to be adapted to the API changes.)

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,7 @@
 """Nox sessions."""
 import contextlib
 import shutil
+import sys
 import tempfile
 from pathlib import Path
 from typing import cast
@@ -13,7 +14,6 @@ from nox.sessions import Session
 package = "retrocookie"
 python_versions = ["3.8", "3.7"]
 nox.options.sessions = "pre-commit", "safety", "mypy", "tests", "typeguard"
-locations = "src", "tests", "noxfile.py", "docs/conf.py"
 
 
 class Poetry:
@@ -130,11 +130,12 @@ def safety(session: Session) -> None:
 @nox.session(python=python_versions)
 def mypy(session: Session) -> None:
     """Type-check using mypy."""
-    args = session.posargs or locations
+    args = session.posargs or ["src", "tests", "docs/conf.py"]
     install_package(session)
     install(session, "mypy")
-    session.install("nox")
     session.run("mypy", *args)
+    if not session.posargs:
+        session.run("mypy", f"--python-executable={sys.executable}", "noxfile.py")
 
 
 @nox.session(python=python_versions)
