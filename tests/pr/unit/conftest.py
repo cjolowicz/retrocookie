c562585d77289a8e11e7f6cfe41ce438198ad2cd
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-12-10 22:16:14 +0100

    Add fixture for fakes.retrocookie

--- tests/pr/unit/conftest.py
+++ tests/pr/unit/conftest.py
@@ -3,9 +3,11 @@ from pathlib import Path
 
 import pytest
 from tests.pr.unit.fakes.github import API as FakeAPI  # noqa: N811
+from tests.pr.unit.fakes.retrocookie import retrocookie
 
 from retrocookie.pr.cache import Cache
 from retrocookie.pr.protocols import github
+from retrocookie.pr.protocols.retrocookie import Retrocookie
 
 
 @pytest.fixture
@@ -18,3 +20,9 @@ def cache(tmp_path: Path) -> Cache:
 def api(tmp_path: Path) -> github.API:
     """Return a fake GitHub API."""
     return FakeAPI(_backend=tmp_path / "github")
+
+
+@pytest.fixture(name="retrocookie")
+def fixture_retrocookie() -> Retrocookie:
+    """Return a fake retrocookie function."""
+    return retrocookie
