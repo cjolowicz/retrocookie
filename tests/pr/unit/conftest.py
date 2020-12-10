bbfa50f84bc7a1376d4d00391eb55ee73cca3ac5
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-12-10 11:48:46 +0100

    Add fixture for github.API

--- tests/pr/unit/conftest.py
+++ tests/pr/unit/conftest.py
@@ -2,11 +2,19 @@
 from pathlib import Path
 
 import pytest
+from tests.pr.unit.fakes.github import API as FakeAPI  # noqa: N811
 
 from retrocookie.pr.cache import Cache
+from retrocookie.pr.protocols import github
 
 
 @pytest.fixture
 def cache(tmp_path: Path) -> Cache:
     """Return a cache."""
     return Cache(tmp_path / "cache")
+
+
+@pytest.fixture
+def api(tmp_path: Path) -> github.API:
+    """Return a fake GitHub API."""
+    return FakeAPI(_backend=tmp_path / "github")
