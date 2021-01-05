353f4b40f79eee45641b9721ae4f99531e75c17c
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2021-01-05 23:15:47 +0100

    Add functions utils.{removeprefix,removesuffix}

--- src/retrocookie/utils.py
+++ src/retrocookie/utils.py
@@ -8,6 +8,16 @@ from typing import Iterator
 from . import git
 
 
+def removeprefix(string: str, prefix: str) -> str:
+    """Remove prefix from string, if present."""
+    return string[len(prefix) :] if string.startswith(prefix) else string
+
+
+def removesuffix(string: str, suffix: str) -> str:
+    """Remove suffix from string, if present."""
+    return string[: -len(suffix)] if suffix and string.endswith(suffix) else string
+
+
 @contextlib.contextmanager
 def chdir(path: Path) -> Iterator[None]:
     """Context manager for changing the directory."""
