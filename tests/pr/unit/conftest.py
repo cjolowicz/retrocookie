cd4741b990d9e1f42fe4e101d33650bd2948a197
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-01-16 22:57:50 +0100

    Add fixture for bus.Bus

--- tests/pr/unit/conftest.py
+++ tests/pr/unit/conftest.py
@@ -1,13 +1,23 @@
 """Fixtures for retrocookie.pr."""
 from pathlib import Path
+from typing import Iterator
 
 import pytest
 from tests.pr.unit.fakes.github import API as FakeAPI  # noqa: N811
 from tests.pr.unit.fakes.retrocookie import retrocookie
 
+from retrocookie.pr.base.bus import Bus
 from retrocookie.pr.cache import Cache
 from retrocookie.pr.protocols import github
 from retrocookie.pr.protocols.retrocookie import Retrocookie
+
+
+@pytest.fixture
+def bus() -> Iterator[Bus]:
+    """Return the bus."""
+    bus = Bus()
+    with bus.events.errorhandler():
+        yield bus
 
 
 @pytest.fixture
